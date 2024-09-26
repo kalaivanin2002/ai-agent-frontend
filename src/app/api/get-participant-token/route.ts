c54f072aaef12c6c93abcaee4a63315ccd0cdf47
kalaivani <kalaivani@agilecybersolutions.com>
2024-09-26 11:37:25 +0530

    updated frontend

--- src/app/api/get-participant-token/route.ts
+++ src/app/api/get-participant-token/route.ts
@@ -1,40 +1,16 @@
-import { AccessToken } from "livekit-server-sdk";
-import { NextRequest, NextResponse } from "next/server";
+import { AccessToken } from 'livekit-server-sdk';
 
-export async function GET(req: NextRequest) {
-
-  const room = req.nextUrl.searchParams.get("room");
-  const username = req.nextUrl.searchParams.get("username");
-  if (!room) {
-    return NextResponse.json(
-      { error: 'Missing "room" query parameter' },
-      { status: 400 }
-    );
-  } else if (!username) {
-    return NextResponse.json(
-      { error: 'Missing "username" query parameter' },
-      { status: 400 }
-    );
-  }
-
+export async function GET(request: Request) {
+  const roomName = Math.random().toString(36).substring(7);
   const apiKey = process.env.LIVEKIT_API_KEY;
   const apiSecret = process.env.LIVEKIT_API_SECRET;
-  const wsUrl = process.env.NEXT_PUBLIC_LIVEKIT_URL;
-
-  // console.log('WebSocket URL:', wsUrl);
-  // console.log('Api Secret', apiSecret)
-  
-
-  if (!apiKey || !apiSecret || !wsUrl) {
-    return NextResponse.json(
-      { error: "Server misconfigured" },
-      { status: 500 }
-    );
-  }
-
-  const at = new AccessToken(apiKey, apiSecret, { identity: username });
-
-  at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
-
-  return NextResponse.json({ token: await at.toJwt() });
+  const at = new AccessToken(apiKey, apiSecret, {identity: "human_user"});
+  at.addGrant({
+    room: roomName,
+    roomJoin: true,
+    canPublish: true,
+    canPublishData: true,
+    canSubscribe: true,
+  });
+  return Response.json({ accessToken: await at.toJwt(), url: process.env.LIVEKIT_URL });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,46 @@
-"use client";
-
+'use client';
 import {
-  ControlBar,
-  GridLayout,
   LiveKitRoom,
-  ParticipantTile,
   RoomAudioRenderer,
-  useTracks,
-} from "@livekit/components-react";
-import "@livekit/components-styles";
-import { Track } from "livekit-client";
-import { useEffect, useState } from "react";
-
-export default function Page() {
-  // TODO: get user input for room and name
-  const room = "quickstart-room";
-  const name = "quickstart-user";
-  const [token, setToken] = useState("");
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const resp = await fetch(
-          `/api/get-participant-token?room=${room}&username=${name}`
-        );
-        if (!resp.ok) {
-          throw new Error(`HTTP error! status: ${resp.status}`);
-        }
-        const data = await resp.json();
-        if (data.error) {
-          throw new Error(data.error);
-        }
-        setToken(data.token);
-        console.log("Received:", data.token);
-      } catch (e) {
-        console.error("Error fetching token:", e);
-        setError(e instanceof Error ? e.message : String(e));
-      }
-    })();
-  }, []);
+  useLocalParticipant,
+} from '@livekit/components-react';
+import { useState } from "react";
 
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (token === "") {
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-    </div>
-  }
+export default () => {
+  const [token, setToken] = useState<string | null>(null);
+  const [url, setUrl] = useState<string | null>(null);
 
   return (
-    <LiveKitRoom
-      // video={true}
-      audio={true}
-      token={token}
-      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
-      // Use the default LiveKit theme for nice styles.
-      data-lk-theme="default"
-      style={{ height: '100dvh' }}
-    >
-      {/* Your custom component with basic video conferencing functionality. */}
-      <MyVideoConference />
-      {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
+    <>
+      <main>
+        {token === null ? (<button onClick={async () => {
+          const {accessToken, url} = await fetch('/api/get-participant-token').then(res => res.json());
+          setToken(accessToken);
+          setUrl(url);
+        }}>Connect</button>) : (
+          <LiveKitRoom
+            token={token}
+            serverUrl={url ?? undefined}
+            connectOptions={{autoSubscribe: true}}
+          >
+            <ActiveRoom />
+          </LiveKitRoom>
+        )}
+      </main>
+    </>
+  );
+};
+
+const ActiveRoom = () => {
+  const { localParticipant, isMicrophoneEnabled } = useLocalParticipant();
+  return (
+    <>
       <RoomAudioRenderer />
-      {/* Controls for the user to start/stop audio, video, and screen
-      share tracks and to leave the room. */}
-      <ControlBar />
-    </LiveKitRoom>
+      <button onClick={() => {
+        localParticipant?.setMicrophoneEnabled(!isMicrophoneEnabled)
+      }}>Toggle Microphone</button>
+      <div>Audio Status: { isMicrophoneEnabled ? 'Unmuted' : 'Muted' }</div>
+    </>
   );
-}
-
-function MyVideoConference() {
-  // `useTracks` returns all camera and screen share tracks. If a user
-  // joins without a published camera track, a placeholder track is returned.
-  const tracks = useTracks(
-    [
-      { source: Track.Source.Camera, withPlaceholder: true },
-      { source: Track.Source.ScreenShare, withPlaceholder: false },
-    ],
-    { onlySubscribed: false },
-  );
-  return (
-    <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
-      {/* The GridLayout accepts zero or one child. The child is used
-      as a template to render all passed in tracks. */}
-      <ParticipantTile />
-    </GridLayout>
-  );
-}
+};
