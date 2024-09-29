63464ff8d8405aaea95db78257ce653db27fb3b7
kalaivani <kalaivani@agilecybersolutions.com>
2024-09-29 12:58:44 +0530

    Update route.ts

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -2,9 +2,10 @@ import { AccessToken } from 'livekit-server-sdk';
 
 export async function GET() {
   const roomName = Math.random().toString(36).substring(7);
+  const identity = Math.random().toString(36).substring(7);
   const apiKey = process.env.LIVEKIT_API_KEY;
   const apiSecret = process.env.LIVEKIT_API_SECRET;
-  const at = new AccessToken(apiKey, apiSecret, {identity: "human_user"});
+  const at = new AccessToken(apiKey, apiSecret, {identity: identity})
   at.addGrant({
     room: roomName,
     roomJoin: true,
